f07c3b3b33c745e11b878a9c043400648026997a
Bruno Purper Farias <brunoplusultra@outlook.com>
2020-11-05 22:08:39 -0300

    up

---
+++ HTML5, CSS3 E JS/Aula-11/js/ex-4.js
@@ -0,0 +1,22 @@
+var nome = 'Cicero Feijó';
+var idade = 39;
+var salario = 1650.76;
+var possuiHabilitacao = true;
+var genero = "";
+var enderecoCompleto = "Rua dos Andradas, 59";
+var sobrenome;
+var par = Number;
+console.log(salario)
+
+var bonus = salario * 0.5;
+var idadeImpar = idade % 2 == 1;
+
+
+// condicionais
+
+if (idade >=18) {
+    console.log('Pode dirigir!');
+} else {
+
+}
+
---
+++ HTML5, CSS3 E JS/Aula-11/js/ex-5.js
@@ -0,0 +1,8 @@
+var alunos = ['Bruno', 'Ana', 'Guilherme' , 'Rodrigo', 'Luiz', 'Isaque'];
+
+for (var cont =0; cont < alunos.length; cont++){
+    
+    alunos.sort()
+    console.log(alunos[cont])
+     
+}
