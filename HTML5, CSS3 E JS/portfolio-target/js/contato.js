22eb04614462183e618a364c9e66c2af9259b5d9
Bruno Purper Farias <brunoplusultra@outlook.com>
2020-11-11 20:12:37 -0300

    a

--- HTML5, CSS3 E JS/portfolio-target/js/contato.js
+++ HTML5, CSS3 E JS/portfolio-target/js/contato.js
@@ -1,11 +1,29 @@
 function validaForm() {
 
     var nome = document.getElementById('nome');
+    var email = document.getElementById('email');
+    var mensagem = document.getElementById('mensagem')
+    var erro = document.getElementById('erro');
 
     if (nome.value == '') {
-        alert('Preencha o nome!');
+        var erro = document.getElementById('erro');
+        erro.innerHTML = 'Preencha o nome *';
         nome.focus();
         return false;
     }
 
+    if (email.value == '') {
+        var erro = document.getElementById('erro');
+        erro.innerHTML = 'Preencha o e-mail *';
+        email.focus();
+        return false;
+    }
+
+    if (mensagem.value == '') {
+        erro.innerHTML = 'Preencha uma mensagem *';
+        mensagem.focus();
+        return false;
+    }
+    
+    return false;
 }
